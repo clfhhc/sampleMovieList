221ba368ec4e94b779978e39f5cfe7d4cd5b4afe
clfhhc <clfhhc@gmail.com>
2018-05-13 16:12:29 -0700

    Refactor the index.js and set up the loader for css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 // src/index.js
 
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './js/components/App.jsx';
-
-let movies = [
-    {title: 'Mean Girls'},
-    {title: 'Hackers'},
-    {title: 'The Grey'},
-    {title: 'Sunshine'},
-    {title: 'Ex Machina'},
-  ];
-
-ReactDOM.render(<App movies={movies} />, document.getElementById('app'));
+import style from "./main.css";
+import index from "./js/index";
---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+// src/js/index.js
+
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+let movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+ReactDOM.render(<App movies={movies} />, document.getElementById('app'));
