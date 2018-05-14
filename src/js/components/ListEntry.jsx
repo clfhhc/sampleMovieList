a88de8105819b86ffb6ed7e9782f19d0aec87ce7
clfhhc <clfhhc@gmail.com>
2018-05-13 17:12:10 -0700

    Changed the CSS style to match the screenshot

--- src/js/components/ListEntry.jsx
+++ src/js/components/ListEntry.jsx
@@ -3,7 +3,7 @@
 import React, {Component} from 'react';
 
 let ListEntry = (props) => (
-    <li>
+    <li className="movie-list-entry">
         <span>
             {props.movie.title}
         </span>
